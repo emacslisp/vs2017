baefd95f0adc3e05e2a48b17ffda2598ba198867
emacslisp <wudi850620@hotmail.com>
2017-04-17 19:00:12 +1000

    add mvc area register

---
+++ RestfulWebApi/RestfulWebApi.Tests/Models/ModelTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestfulWebApi;
+using RestfulWebApi.Controllers;
+using RestfulWebApi.Models;
+
+namespace RestfulWebApi.Tests.Models
+{
+    [TestClass]
+    public class ModelsTest
+    {
+        [TestMethod]
+        public void ContactModelTest()
+        {
+            Contact contact = new Contact() { Id=1,Name="Test" };
+
+            Assert.AreEqual(contact.Id, 1);
+            Assert.AreEqual(contact.Name, "Test");
+        }
+    }
+}
--- RestfulWebApi/RestfulWebApi/Models/Contact.cs
+++ RestfulWebApi/RestfulWebApi/Models/Contact.cs
@@ -10,7 +10,7 @@ namespace RestfulWebApi.Models
         private int id;
         private string name;
 
-        public int Id { get => id; set => id = value; }
-        public string Name { get => name; set => name = value; }
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get { return name; } set { name = value; } }
     }
 }
